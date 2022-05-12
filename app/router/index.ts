1e59c3962ca1f6e1b6589c5f95fd276e3e473d8a
mac-Zeyull <13508085664@163.com>
2022-05-13 03:38:42 +0800

    引入了JWT中间层判断

--- app/router/index.ts
+++ app/router/index.ts
@@ -10,28 +10,24 @@ const router = new koaRouter({prefix:"/api"});
 router.post('/register',LoginController.register);
 router.post('/getCaptcha',indexController.getCaptcha);
 router.post('/login',LoginController.login);
-router.get('/getUserInfo',UserController.getUserInfo);
-router.post('/updateUserInfo',UserController.updateUserInfo);
-router.post('/uploadHeaderImage',UploadController.uploadHeaderImage);
-
-//code部分
 router.get('/getAllFrontQuestion',CodeController.getAllFrontQuestion);
-router.post('/createFrontQuestion',CodeController.createFrontQuestion);
-router.post('/judgeCode',CodeController.judgeCode);
-router.get('/getQuestionByID',CodeController.getQuestionByID);
 router.get('/getAllQuestion',CodeController.getAllQuestion);
-router.get('/getJudgeSubmitsByID',CodeController.getJudgeSubmitsByID);
-router.get('/getSubHistoryByID',CodeController.getSubHistoryByID);
 router.get('/getDailyQuestion',CodeController.getDailyQuestion);
-
-//article部分
-router.post('/createArticle',ArticleController.createArticle);
-router.get('/getArticleByID',ArticleController.getArticleByID);
-router.post('/getArticleLikeOrNot',ArticleController.getArticleLikeOrNot);
-router.post('/addComment',ArticleController.addComment);
 router.get('/getAllTags',ArticleController.getAllTags);
 router.get('/getAllArticle',ArticleController.getAllArticle);
+router.get('/getQuestionByID',CodeController.getQuestionByID);
+router.get('/getSubHistoryByID',CodeController.getSubHistoryByID);
+router.get('/getArticleByID',ArticleController.getArticleByID);
+router.get('/getUserInfo',UserController.getUserInfo);
+router.get('/getJudgeSubmitsByID',CodeController.getJudgeSubmitsByID);
+// token 判断
+router.use(AuthMiddleware);
+router.post('/updateUserInfo',UserController.updateUserInfo);
+router.post('/uploadHeaderImage',UploadController.uploadHeaderImage);
+router.post('/createFrontQuestion',CodeController.createFrontQuestion);
+router.post('/judgeCode',CodeController.judgeCode);
+router.post('/createArticle',ArticleController.createArticle);
+router.post('/getArticleLikeOrNot',ArticleController.getArticleLikeOrNot);
+router.post('/addComment',ArticleController.addComment);
 router.delete('/deleteArticle',ArticleController.deleteArticle);
-
-// router.use(AuthMiddleware)
 export default router;
